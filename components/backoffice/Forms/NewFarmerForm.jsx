5ab2e2d71022ad2e5c3be5ff493703023813a234
Ammar-Ahmed30 <61118757+Ammar-Ahmed30@users.noreply.github.com>
2024-08-10 23:01:24 +0300

    sdsds

--- components/backoffice/Forms/NewFarmerForm.jsx
+++ components/backoffice/Forms/NewFarmerForm.jsx
@@ -39,7 +39,7 @@ export default function NewFarmerForm({ updateData = {}, user = {} }) {
   });
   const router = useRouter();
 
-  const id = updateData?.userId ?? '';
+  const id = user?.id;
   console.log('id : ', id);
   const isActive = watch('isActive');
 
