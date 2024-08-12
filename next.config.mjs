7d1eace6875a36295982a2508f875004ccfb013f
Ammar-Ahmed30 <61118757+Ammar-Ahmed30@users.noreply.github.com>
2024-08-12 17:56:39 +0300

    hhhh

--- next.config.mjs
+++ next.config.mjs
@@ -12,34 +12,34 @@ const nextConfig = {
       },
     ],
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       // Matching all API routes
-  //       source: '/api/:path*',
-  //       headers: [
-  //         { key: 'Access-Control-Allow-Credentials', value: 'true' },
-  //         {
-  //           key: 'Access-Control-Allow-Origin',
-  //           value: '*', // Replace with your actual origin
-  //         },
-  //         {
-  //           key: 'Access-Control-Allow-Methods',
-  //           value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS', // Include OPTIONS for preflight requests
-  //         },
-  //         {
-  //           key: 'Access-Control-Allow-Headers',
-  //           value:
-  //             'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, X-Uploadthing-Header',
-  //         },
-  //         {
-  //           key: 'Access-Control-Max-Age',
-  //           value: '86400', // Cache the preflight response for a day
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        // Matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // Replace with your actual origin
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS', // Include OPTIONS for preflight requests
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, X-Uploadthing-Header',
+          },
+          {
+            key: 'Access-Control-Max-Age',
+            value: '86400', // Cache the preflight response for a day
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
