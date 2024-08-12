0c96b79a7be09096de688f2ac62be258603753a2
Ammar-Ahmed30 <61118757+Ammar-Ahmed30@users.noreply.github.com>
2024-08-12 20:21:30 +0300

    llll

--- app/(back-office)/dashboard/community/page.jsx
+++ app/(back-office)/dashboard/community/page.jsx
@@ -11,7 +11,7 @@ export default async function Trainings() {
   return (
     <div className="flex flex-col gap-6 px-4 mx-auto max-w-[325px] xs:max-w-[480px] sm:max-w-[650px] md:max-w-[1000px]  lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl 3xl:max-w-3xl 4xl:max-w-4xl 5xl:max-w-5xl 6xl:max-w-6xl  overflow-auto">
       <PageHeader
-        heading="Bazar Community Trainings"
+        heading="Trainings"
         href="/dashboard/community/new"
         linkTitle="Add Training"
       />
